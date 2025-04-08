3f7779079f5657fb9e403f635852801792aedefe
SamanthaLProgramming <150631576+SamanthaLProgramming@users.noreply.github.com>
2025-04-07 18:54:13 -0700

    fix: padding on form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -41,7 +41,7 @@ export default function Login() {
   };
 
   return (
-    <section className="bg-img min-h-screen bg-[url('/greenlandscape-full.jpg')] bg-cover bg-center bg-no-repeat p-4">
+    <section className="bg-img min-h-screen bg-[url('/greenlandscape-full.jpg')] bg-cover bg-center bg-no-repeat px-4">
       <div className="z-2 relative m-auto translate-y-1/2 rounded-xl border-2 border-white/40 bg-transparent px-8 py-10 text-white shadow-xl backdrop-blur-md [transition:.5s_ease] md:w-1/2 xl:w-1/3">
         <h2 className="text-center text-4xl font-semibold uppercase">Login</h2>
         <form onSubmit={handleLogin} aria-labelledby="login-form">
