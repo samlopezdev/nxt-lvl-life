2c70d011b1041c4649888627c286d41928a1e88f
SamanthaLProgramming <150631576+SamanthaLProgramming@users.noreply.github.com>
2025-11-04 14:37:13 -0800

    fix: remove debug logs from login function

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -13,8 +13,6 @@ export default function Login() {
     setIsLoading(true);
 
     try {
-      console.log("Issue here -sad face-")
-      console.log(import.meta.env.VITE_BASE_URL)
       const response = await fetch(
         import.meta.env.VITE_BASE_URL + "/auth/login",
         {
